7e87248d37b87224d91f7ade0f7a946981d9a745
SabineSmulders <92474494+SabineSmulders@users.noreply.github.com>
2022-01-04 21:34:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const age = 18; ("Hoe oud ben je?")
+if (age > 17) {
+
+    console.log("Je kan verder")
+
+} else {
+    console.log("Je kan geen ticket kopen")
+}
+if (age = 18 && 25) {
+
+    console.log("You get 50% off!")
+} else {
+    console.log("Je kan geen gebruik maken van deze korting")
+}
+
+const isFemale = true
+if (isFemale) {
+
+    console.log("Je kan een ticket kopen")
+
+} else {
+    console.log("Je kan helaas geen ticket kopen")
+}
+
+const driverStatus = designated
+if (driverStatus = designated) {
+
+    console.log("You're allowed to drive home")
+
+} else {
+    console.log("You're not allowed to drive")
+}
+
+const firstname = Sarah || Abraham
+if (firstname = Sarah || Abraham) {
+
+    console.log("You get a free beer on this festive day")
+} else {
+    console.log("Sorry, you gotta pay")
+}
+
+const totalAmount = 25
+if (totalAmount > 25) {
+    console.log("Free veggie bitterballen")
+}
+const totalAmount = 50
+if (totalAmount > 50) {
+    console.log("Free nachos")
+}
+const totalAmount = 50
+if (totalAmount > 50) {
+    console.log("Free champagne")
+}
